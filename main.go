252cec966de298cb8f80e713fb3f3d1a53b3c114
MaurickThom <thomtwd@gmail.com>
2019-09-18 01:29:05 -0500

    Adding comments to the handler

--- main.go
+++ main.go
@@ -8,15 +8,19 @@ import (
 )
 
 func main() {
-	e := echo.New()
+	var e = echo.New()
 	e.GET("/", handlerHelloWorld)
-	err := e.Start(":8080")
+	var err = e.Start(":8080")
 	if err != nil { // nil significa nulo , entonces se le pregunta si error
 		// esta nulo o no, si no esta nulo significa que hay un error
 		fmt.Printf("No pude subir el servidor %v", err)
 	}
 }
 
+/*
+con el framework echo necesito el contexto
+en el contexto me trae el request y el response
+*/
 func handlerHelloWorld(context echo.Context) error {
 	return context.String(http.StatusOK, "Hello world")
 }
