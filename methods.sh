a1ea5642cb6f9780eab7caa88b701eac9c883f2c
MaurickThom <thomtwd@gmail.com>
2019-09-18 13:39:45 -0500

    update curl sh

--- methods.sh
+++ methods.sh
@@ -1,1 +1,2 @@
-curl -d '{"brand":"walon", "price":"1000","color":"blue"}' -H "Content-Type: application/json" -X POST http://localhost:8080/shoes | jq
+# jajajajja walon jajjjajajajaja chavex xdxd jajajajaj 
+curl -d '{"brand":"walon", "price":1000,"color":"blue"}' -H "Content-Type: application/json" -X POST http://localhost:8080/shoes | jq
